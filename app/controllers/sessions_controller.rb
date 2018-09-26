159244bd4b62c0ddaa3ca91a666bcdd1dcfc5ab6
enciie <enciie@gmail.com>
2018-09-25 23:09:42 -0700

    Add sessions#new, #create, and #destroy action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,24 @@
 class SessionsController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:user][:username])
+    #If user exists AND password is correct
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      # Sends user back to the login form
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
   end
-  
+
 end
