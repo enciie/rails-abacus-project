18deb40f364f5b02578643444f11a72553bdc06a
enciie <enciie@gmail.com>
2018-09-26 01:26:12 -0700

    Add index action to groups controller and comments

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,2 +1,8 @@
 class GroupsController < ApplicationController
+
+  def index
+    @group = Group.new
+    @groups = Group.all 
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #app homepage
   root to: "application#home"
 
+  #Signup Page
   get '/signup' => 'users#new'
+
+  #Routes for user login form, logging user in, and logging user out
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
-  get '/logout' => 'sessions#destroy'
+  delete '/logout' => 'sessions#destroy'
 
   resources :users, only: [:create, :show, :edit, :update]
-  
+
 end
