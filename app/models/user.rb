a18add2bd2757e528edd2c5ab518ec67f4cb6c47
enciie <enciie@gmail.com>
2018-09-25 19:43:42 -0700

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ApplicationRecord
   has_many :categories, :through => :expenses
 
   has_secure_password
+
+  # Validations
+  validates :name, presence: true,
+                   uniqueness: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[A-Za-z0-9._%+-]+@[A-Za-z0-9\.-]+\.[A-Za-z]+\Z/ }
+  validates :password, presence: true
 end
