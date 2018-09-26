9d9a9201418a0e885d5098b77ffdc86ca0b8980c
enciie <enciie@gmail.com>
2018-09-25 22:35:48 -0700

    Generate sessions controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "application#home"
 
-  resources :users
+  get '/signup' => 'users#new'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  resources :users, only: [:create, :show, :edit, :update]
+  
 end
