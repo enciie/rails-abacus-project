b5a22b97185db25d3feccc49ffe4532e312f46fe
enciie <enciie@gmail.com>
2018-09-26 01:33:07 -0700

    Add comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,8 @@ class ApplicationController < ActionController::Base
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
   helper_method :current_user
+  # methods you build in controller do not permeate to your ActionView
+  # you have to explicitly tell it by calling it a helper method
 
   def require_login
     redirect_to root_path unless logged_in?
