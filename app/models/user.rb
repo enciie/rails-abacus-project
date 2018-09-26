9385c4fff6e1c5f7462e35d06a7713f8395848cb
enciie <enciie@gmail.com>
2018-09-25 19:45:33 -0700

    Change name attribute to username

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   has_secure_password
 
   # Validations
-  validates :name, presence: true,
+  validates :username, presence: true,
                    uniqueness: true
   validates :email, presence: true,
                     uniqueness: true,
